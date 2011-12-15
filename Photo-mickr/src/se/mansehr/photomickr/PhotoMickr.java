b4782869be3147766c35414ef85d61076bcd096d
sehr.andreas <sehr.andreas@gmail.com>
2011-12-15 23:17:13 +0000

    Updated this file

--- Photo-mickr/src/se/mansehr/photomickr/PhotoMickr.java
+++ Photo-mickr/src/se/mansehr/photomickr/PhotoMickr.java
@@ -6,8 +6,7 @@ public class PhotoMickr {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+    System.out.println("Hello Stockholm");
 	}
 
 }
