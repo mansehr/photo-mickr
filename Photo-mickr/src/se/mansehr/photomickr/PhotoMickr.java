2092a69080b999c7f58d4c77f00af8bcf3c6fab8
Andreas Sehr <sehr.andreas@gmail.com>
2011-12-16 00:12:31 +0100

    First test

---
+++ Photo-mickr/src/se/mansehr/photomickr/PhotoMickr.java
@@ -0,0 +1,13 @@
+package se.mansehr.photomickr;
+
+public class PhotoMickr {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
